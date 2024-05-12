7868b6474cf11884a3a2dcb8090b73194a883beb
Sam559I <samirul559@live.com>
2024-05-12 16:52:03 +1000

    add package Pip rich to main.py

--- src/main.py
+++ src/main.py
@@ -6,26 +6,52 @@ Users can display current stock, search for specific products, order groceries, 
 expired perishable food items, and compare prices.
 
 """
+import datetime
+from rich.console import Console
+from grocery import load_grocery_items, display_current_stock, adjust_groceries
 from search import search_product
 import csv
-from grocery import (
-    load_grocery_items,
-    display_current_stock,
-    quantity_update_groceries,
-    display_items_by_category,
-    track_expiring_soon,
-)
+from rich.table import Table
+import os
 
-grocery_items = []
+# directory of the current script
+script_dir = os.path.dirname(os.path.abspath(__file__))
+
+marketplace1 = os.path.join(script_dir, "Marketplace-1.csv")
+marketplace2 = os.path.join(script_dir, "Marketplace-2.csv")
+marketplace3 = os.path.join(script_dir, "Marketplace-3.csv")
+
+console = Console()
+
+
+def track_expiring_soon():
+    today = datetime.date.today()
+    six_months_from_now = today + datetime.timedelta(days=30 * 6)
+
+    # Load grocery items
+    grocery_items = load_grocery_items()
+
+    expiring_items = []
+    for item in grocery_items:
+        if "Expiration Date" in item:
+            expiration_date = datetime.datetime.strptime(
+                item["Expiration Date"], "%d/%m/%Y"
+            ).date()
+            if today <= expiration_date <= six_months_from_now:
+                expiring_items.append(item)
+
+    if expiring_items:
+        console.print("\nItems Expiring in the Next 6 Months:")
+        for item in expiring_items:
+            console.print(f"{item['Name']} - Expires on {item['Expiration Date']}")
+    else:
+        console.print("No items are expiring in the next 6 months.")
+
 
 def compare_prices_for_product():
     global grocery_items
-
+    grocery_items = load_grocery_items()
     product_name = input("Enter the name of the product to compare prices: ")
-
-    marketplace1 = "Marketplace-1.csv"
-    marketplace2 = "Marketplace-2.csv"
-    marketplace3 = "Marketplace-3.csv"
 
     marketplace_prices1 = {}
     marketplace_prices2 = {}
@@ -46,39 +72,91 @@ def compare_prices_for_product():
         for row in reader:
             marketplace_prices3[row["Name"]] = float(row["Price"])
 
-    print(f"\nPrice Comparison for '{product_name}' with Marketplaces:")
+    console.print(f"\nPrice Comparison for '{product_name}' with Marketplaces:")
     for item in grocery_items:
-        if item["Name"].lower() == product_name.lower():
-            price1 = marketplace_prices1.get(item["Name"], "Not Available")
-            price2 = marketplace_prices2.get(item["Name"], "Not Available")
-            price3 = marketplace_prices3.get(item["Name"], "Not Available")
 
-            print(
-                f"{item['Name']}: Our Price - ${item['Price']} | Marketplace 1 Price - ${price1} | Marketplace 2 Price - ${price2} | Marketplace 3 Price - ${price3}"
-            )
-            return
+        price1 = marketplace_prices1.get(item["Name"], "Not Available")
+        price2 = marketplace_prices2.get(item["Name"], "Not Available")
+        price3 = marketplace_prices3.get(item["Name"], "Not Available")
 
-    print(f"No product named '{product_name}' found in the current stock.")
+        console.print(
+            f" Marketplace 1 Price - ${price1} | Marketplace 2 Price - ${price2} | Marketplace 3 Price - ${price3}"
+        )
+        return
+
+    console.print(f"No product named '{product_name}' found in the current stock.")
 
 
-# Main function
+def display_items_by_category():
+    grocery_items = load_grocery_items()  # Load grocery items here
+    category = input("Enter the category to display items: ").strip().capitalize()
+    table = Table(title=f"Grocery Items in the Category '{category}'")
+    table.add_column("Name")
+    table.add_column("Quantity")
+    table.add_column("Price")
+
+    category_items = [
+        item for item in grocery_items if item["Category"].capitalize() == category
+    ]
+    if category_items:
+        for item in category_items:
+            table.add_row(item["Name"], item["Quantity"], f"${item['Price']}")
+    else:
+        console.print("No items found in the specified category.")
+
+    console.print(table)
+
+
+def place_order():
+    total_cost = 0
+    console.print("\nPlace an Order:")
+    marketplace_number = input("Enter the marketplace number to order from: ")
+    marketplace_file = os.path.join(script_dir, f"Marketplace-{marketplace_number}.csv")
+
+    # Load grocery items from the selected marketplace CSV file
+    marketplace_items = []
+    with open(marketplace_file, mode="r") as file:
+        reader = csv.DictReader(file)
+        for row in reader:
+            marketplace_items.append(row)
+
+    num_items = int(input("Enter the number of items: "))
+    for i in range(num_items):
+        item_name = input(f"Enter the name of item {i + 1}: ")
+        quantity = int(input(f"Enter the quantity of {item_name}: "))
+
+        # Check if the requested quantity is greater than the available quantity
+        for item in marketplace_items:
+            if item["Name"].lower() == item_name.lower():
+                available_quantity = int(item["Quantity"])
+                if quantity > available_quantity:
+                    console.print(
+                        f"Error: The requested quantity ({quantity}) for '{item_name}' exceeds the available quantity ({available_quantity})."
+                    )
+                    return
+
+                total_cost += float(item["Price"]) * quantity
+                break
+
+    console.print(
+        f"\nTotal cost of the order from Marketplace {marketplace_number}: ${total_cost:.2f}"
+    )
+
+
 def main():
     load_grocery_items()
     while True:
-        print("\n")
-        print("╔══════════════════════════════════════════════════════════════════════════╗")
-        print("║                       Welcome to the Grocery                             ║")
-        print("║                             Tracker App!                                 ║")
-        print("╚══════════════════════════════════════════════════════════════════════════╝")
-        print("Menu Options:")
-        print("1. Display Current Stock")
-        print("2. Search for a Product")
-        print("3. Update Groceries")
-        print("4. Track Expired Perishable Food Items")
-        print("5. Price Comparison")
-        print("6. Display Items by Category")
-        print("7. Place Order")
-        print("8. Exit")
+        console.print("\n")
+        console.print("[bold green]Welcome to the Grocery Tracker App![/bold green]")
+        console.print("Menu Options:")
+        console.print("1. Display Current Stock")
+        console.print("2. Search for a Product")
+        console.print("3. Update Groceries")
+        console.print("4. Track Expired Perishable Food Items")
+        console.print("5. Price Comparison")
+        console.print("6. Display Items by Category")
+        console.print("7. Place Order")
+        console.print("8. Exit")
         choice = input("Enter your choice: ")
         try:
             choice = int(choice)
@@ -88,7 +166,7 @@ def main():
                 product_name = input("Enter the name of the product to search: ")
                 search_product(product_name)
             elif choice == 3:
-                quantity_update_groceries()
+                adjust_groceries()
             elif choice == 4:
                 track_expiring_soon()
             elif choice == 5:
@@ -98,12 +176,12 @@ def main():
             elif choice == 7:
                 place_order()
             elif choice == 8:
-                print("\nThank you for using the Grocery Tracker App. Goodbye!")
+                console.print("\nThank you for using the Grocery Tracker App. Goodbye!")
                 break
             else:
-                print("Invalid choice. Please try again.")
+                console.print("Invalid choice. Please try again.")
         except ValueError:
-            print("Invalid input. Please enter a number.")
+            console.print("Invalid input. Please enter a number.")
 
 
 if __name__ == "__main__":
